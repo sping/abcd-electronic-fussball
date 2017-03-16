4460b72359eb3ab863c0d72bcecae91a64dc46da
Bing Steup <b.steup@sping.nl>
2017-03-16 12:11:19 +0100

    Add dev dependencies

---
+++ api/shipitfile.js
@@ -0,0 +1,48 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+  require('shipit-pm2')(shipit);
+  require('shipit-npm')(shipit);
+  require('shipit-shared')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: 'tmp',
+      deployTo: '/root/www/abcdef-api',
+      repositoryUrl: 'git@github.com:sping/abcd-electronic-fussball.git',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 10,
+      shallowClone: true,
+      dirToCopy: 'api',
+      npm: {
+        remote: true,
+        installFlags: ['--only=production']
+      },
+      shared: {
+        overwrite: false,
+        files: [
+          'config/config.json',
+          'config/env.json',
+          'app.json'
+        ],
+      }
+    },
+    staging: {
+      branch: 'develop',
+      servers: 'root@adleman.servers.sping.nl'
+    },
+    production: {
+      branch: 'master',
+      servers: 'root@adleman.servers.sping.nl'
+    }
+  });
+
+  shipit.blTask('db:migrate', function() {
+    return shipit.remote('cd /var/www/recipe-book-api/current && sequelize db:migrate');
+  });
+
+  shipit.on('published', function() {
+    return shipit.start('db:migrate');
+  });
+};
+
+
