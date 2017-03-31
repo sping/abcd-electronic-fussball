37aa07a03c7e50f3732be7e7416e313b67500dd2
Bing Steup <b.steup@sping.nl>
2017-03-31 13:20:31 +0200

    Add test-env.json for codeship

--- api/config/env.js
+++ api/config/env.js
@@ -1,6 +1,10 @@
 jsonfile = require('jsonfile');
 
 file = __dirname + '/env.json';
+if ( typeof __TEST__ !== 'undefined' && __TEST__ ) {
+  file = __dirname + '/env-test.json';
+}
+
 env = jsonfile.readFileSync(file);
 
 
