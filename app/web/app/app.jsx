69a49d9e30f8f71d3949f7f57d0aaed13dd60c4c
Bing Steup <b.steup@sping.nl>
2017-03-31 14:16:21 +0200

    Add fastclick Only fire JS errors to sentry in production

--- app/web/app/app.jsx
+++ app/web/app/app.jsx
@@ -8,6 +8,19 @@ import Routes from './router';
 import Tabbar from './views/Tabbar';
 import './stylesheets/app.sass';
 
+// Track errors in production env
+if (process.env.NODE_ENV === 'production') {
+  Raven.config('https://4ef1929be17b4868b4328e552d1451ad@sentry.io/152218').install()
+}
+
+// remove 200ms delay on mobile click
+import FastClick from 'fastclick'
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
+}
+
 let store = configureStore()
 
 // Render it to DOM
