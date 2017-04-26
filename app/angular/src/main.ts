9759fb0a70b3e37a897031f8aa6f04963c428d4a
Glenn Vlekke <g.vlekke@sping.nl>
2017-04-26 11:30:45 +0200

    eject

--- app/angular/src/main.ts
+++ app/angular/src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+OfflinePluginRuntime.install();
 
 if (environment.production) {
   enableProdMode();
